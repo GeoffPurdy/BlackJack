dcca3c8250176d6e7de2488e0237ff52b8bbc35b
GeoffPurdy <geoff_purdy@yahoo.com>
2014-09-21 22:14:06 -0400

    for submission

--- lib/BlackJack.rb
+++ lib/BlackJack.rb
@@ -1,49 +1,37 @@
-class Bet
-  attr_accessor :value
-
-  def initialize(value)
-    @value = value
-  end
-end
-
-class Chip
-  attr_accessor :color
-  attr_accessor :value
-
-  def initialize(color, value)
-    @color = color
-    @value = value
-  end
-end
-
 class Card
-  attr_accessor :suit
-  attr_accessor :rank
+  # Unicode 6.0+ Playing Cards http://www.unicode.org/charts/PDF/U1F0A0.pdf
+  # 0x1F0A..0x1F0D by 0x1..0xE matrix
   attr_accessor :is_visible
+  attr_reader   :hex_code
 
-  def initialize(rank, suit)
-    @rank = rank
-    @suit = suit
+  def initialize(hex_code)
     @is_visible = false
+    @hex_code = hex_code
   end
 
   def value
-    return @is_visible ? @value : nil
+    val = @hex_code % 0x10        # ones hex digit of unicode code is rank
+    return val <= 0x9 ? val : 0xA # translate rank to value
   end
 end
 
 class Deck
   attr_reader :is_shuffled
 
-  def initialize(num_cards)
-    @CARD_VALUES = [1,2,3,4,5,6,7,8,9,10,10,10,10] #A,1,2,3,4,5,6,7,8,9,10,J,Q,K
-    # populate the deck
-    @deck_values = []
-    (num_cards / @CARD_VALUES.size).times do @deck_values.push(*@CARD_VALUES) end
-    @num_cards = num_cards
+  def initialize
     @is_shuffled = false
     @cards = []
-    @num_cards.times { |n| @cards[n] = Card.new(n, "x") }
+
+    # iterate over 2D array of hex prefixes and suffixes to build unicode deck
+    cols = (0x1F0A..0x1F0D) # Hex prefix for unicode 6.0+ playing card suit
+    rows = (0x1..0xE).to_a  # Hex suffix for unicode 6.0+ playing card rank
+    rows.delete(0xC)        # C = unicode Knight We don't use it. Delete!
+
+    cols.each do |i|
+      rows.each do |j|
+        @cards << Card.new((i.to_s(16) + j.to_s(16)).hex) # strcat & conv to hex
+      end
+    end
   end
 
   def empty?
@@ -68,41 +56,47 @@ end
 
 class Strategy
   # trivial implementation of interface
-  def bet
-    return 1
+  def initialize(players)
+    @players = players
   end
+
+  def bet
+    return 10
+  end
+
   def hit?
     return false
   end
 end
 
 class DealerStrategy < Strategy
-  def bet
-    return 0
-  end
-end
-
-class BrainlessStrategy < Strategy
-  def bet
-    return 2
-  end
+  # to be implemented w/ dealer rules & constraints
   def hit?
-    return true
+    dealer = @players.first.show_hand
+    opponent = @players.last.show_hand
+    return (opponent < 22 && opponent > dealer && dealer < 17)
   end
 end
 
-class PunyHumanStrategy < Strategy
+class RandomStrategy < Strategy
   def bet
-    @amt = nil
+    return rand(1..10)
+  end
+
+  def hit?
+    return rand(1).zero?
+  end
+end
+
+class InteractiveStrategy < Strategy
+  def bet
     puts "Puny Human bets 10.  House rules."
-#    while (@amt and @amt.to_i != 0 and "0" != @amt)
-#      @amt = gets.chomp
-#    end
-    return 10 # @amt.to_i
+    return 10
   end
 
   def hit?
-    @ans = nil
+    p "Dealer has: " + @players.first.show_hand.to_s
+    p "You have: "   + @players.last.show_hand.to_s
     p "Would you like to hit? Type 'y' to hit, anything else to stay: "
     @ans = gets.chomp.downcase
     return @ans =~ /^y/
@@ -133,7 +127,8 @@ class Player
 
   def show_hand
     sum = 0
-    return @hand.size { |h| sum += @hand[h].rank }
+    @hand.each { |h| sum += h.value }
+    return sum
   end
 
   def destroy_hand
@@ -147,31 +142,34 @@ class Player
   def exchange_money(amount)
     @money += amount
   end
+
+  def has_money?
+    return @money > 0
+  end
 end
 
-class Game
+class BlackJack
   def initialize(blackjack, num_players)
     @BLACKJACK = blackjack
     @CARDS_IN_DECK = 52
-    @players = []
-    @deck = Deck.new(@CARDS_IN_DECK)
+    @deck = Deck.new
     @pot = 0
+    @players = []
     @table = []
 
     num_players.times { |n|
       if(0 == n) # first player is arbitrarily dealer
-        @players[n] = Player.new(true, DealerStrategy.new, 100)
+        @players[n] = Player.new(true, DealerStrategy.new(@players), 100)
       else
-        @players[n] = Player.new(true, PunyHumanStrategy.new, 100)
+        @players[n] = Player.new(false, InteractiveStrategy.new(@players), 100)
       end
     }
-
   end
 
   def play
     @deck.shuffle!
     # create players
-    while @deck.has_cards?(@players.size)
+    while (@deck.has_cards?(@players.size) && @players.last.has_money?)
       # each player gets a card
       @players.each{ |player| player.save_card(@deck.pop) }
       # each player places a bet
@@ -185,18 +183,22 @@ class Game
       # each player reveals cards
       @players.each{ |player|
         score = player.show_hand
-        # score over blackjack => Busted!
+        # score over blackjack => Busted => lose bet
         score > @BLACKJACK ? @table.push(0) : @table.push(player.show_hand)
         player.destroy_hand
       }
       # winner collects pot; taking index works only b/c dealer goes first
-      @players[ @table.index(@table.max) ].exchange_money(@pot)
+      winner = @table.index(@table.max)
+      print "Player " + winner.to_s + " wins $" + @pot.to_s + "\n\n\n\n\n"
+      @players[ winner ].exchange_money(@pot) # FIXME this seems problematic
       @pot = 0
+      @table = [] # results don't carry over => clear table
     end
   end
 
   def results
-    return @players.inspect
+    return "Dealer: $" + @players.first.money.to_s +
+    "\nPlayer: $" + @players.last.money.to_s
   end
 
   def to_s
@@ -205,14 +207,9 @@ class Game
 
 end
 
-class BlackJack
-  def run
-    fail "You need to add some functionality here before you can use this."
-  end
-end
 
-g = Game.new(21,2)
+bj = BlackJack.new(21,2)
 puts "starting game"
-g.play
+bj.play
 puts "game over"
-print g.results + "\n\n\n"
+print bj.results + "\n\n\n"
--- lib/playing_cards.rb
+++ lib/playing_cards.rb
@@ -1,17 +1,15 @@
 #Encoding: UTF-8
 
-# 1F0A 1F0B 1F0C 1F0D
-# 0
-# .
-# .
-# E
-ary = []
+# Unicode 6.0+ Playing Cards http://www.unicode.org/charts/PDF/U1F0A0.pdf
+# 0x1F0A..0x1F0D by 0x1..0xE matrix
+
+deck = []
 cols = (0x1F0A..0x1F0D)
 rows = (0x1..0xE).to_a
 rows.delete(0xC) # C = Knight LOL  Delete it!
 
 cols.each do |i|
   rows.each do |j|
-    ary << (i.to_s(16) + j.to_s(16)).hex
+    deck << (i.to_s(16) + j.to_s(16)).hex
   end
 end
